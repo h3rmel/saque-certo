baf88fca4d57459f3a4778332ab7496fcd110543
Isaac Hermel <isaachermel@gmail.com>
2025-05-21 14:44:51 -0300

    fix: remove unused header component

--- src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-import { useScrollPosition } from "@/hooks/guarahooks/use-scroll-position";
-import { cn } from "@/lib/utils";
-
-export function Header() {
-  const { x, y } = useScrollPosition();
-
-  return <header className={cn()}></header>;
-}
